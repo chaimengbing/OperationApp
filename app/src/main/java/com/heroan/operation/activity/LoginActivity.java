fc6befb04d4c36bf614868d811e1cd6a143ebfa5
chaimengbing <chaimengbing@qinqinbaby.com>
2019-02-19 10:33:23 +0800

    登录

---
+++ app/src/main/java/com/heroan/operation/activity/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.heroan.operation.activity;
+
+import android.os.Bundle;
+
+import zuo.biao.library.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+}
